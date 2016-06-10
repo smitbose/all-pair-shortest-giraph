0a39e5802f8f0ac3e1f74564c1db107281296a2a
smitbose <bosmit90@gmail.com>
2016-06-10 05:37:23 +0530

    Added Computation structure

--- AllShortestPath/src/core/MyComputation.java
+++ AllShortestPath/src/core/MyComputation.java
@@ -1,5 +1,21 @@
 package core;
 
-public class MyComputation {
+import java.io.IOException;
+
+import graph.VertexVal;
+
+import org.apache.giraph.graph.BasicComputation;
+import org.apache.giraph.graph.Vertex;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+
+public class MyComputation extends BasicComputation<IntWritable, VertexVal, NullWritable, NullWritable>{
+
+	@Override
+	public void compute(Vertex<IntWritable, VertexVal, NullWritable> arg0,
+			Iterable<NullWritable> arg1) throws IOException {
+		
+		
+	}
 
 }
